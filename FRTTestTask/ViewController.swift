a86fbd1854fd92241975135b4fa32e32310f4a38
Vahe Hakobyan <vahe.g.hakobyan@gmail.com>
2022-04-16 20:49:11 +0400

    Initial commit

---
+++ FRTTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FRTTestTask
+//
+//  Created by Vahe Hakobyan on 16.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
